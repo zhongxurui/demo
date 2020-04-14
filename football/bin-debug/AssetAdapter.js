fc7525708af230dd2128f59a01fbca1f4e17cbf1
zhongxurui_mac <zhongxurui@xdf.cn>
2020-04-14 16:38:48 +0800

    1

--- football/bin-debug/AssetAdapter.js
+++ football/bin-debug/AssetAdapter.js
@@ -1,6 +1,6 @@
 //////////////////////////////////////////////////////////////////////////////////////
 //
-//  Copyright (c) 2014-present, Egret Technology.
+//  Copyright (c) 2014-present, Egret T1111echnology.
 //  All rights reserved.
 //  Redistribution and use in source and binary forms, with or without
 //  modification, are permitted provided that the following conditions are met:
